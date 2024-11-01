b992ca47c439f15c0385b23ab964f1bf8c525ea0
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2024-10-31 22:54:23 -0400

    setup esp stuff with espidf

---
+++ esp_control/main/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+#define LED_PIN 13
+
+void app_main() {
+    // Configure the LED pin
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << LED_PIN),
+        .mode = GPIO_MODE_OUTPUT,
+    };
+    gpio_config(&io_conf);
+
+    while (1) {
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
