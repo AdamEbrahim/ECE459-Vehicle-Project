f90b198bf37ce36f0fcf6826eaf48005f718ff09
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2024-11-09 15:52:33 -0500

    yes

--- esp_control/main/main.c
+++ esp_control/main/main.c
@@ -10,11 +10,16 @@
 
 #include <driver/i2c.h>
 
+//pwm drivers
+#include <driver/ledc.h>
+#include <math.h>
+
 #define LED_PIN 13
 #define UT_TRIGGER_PIN 26
 #define UT_ECHO_PIN 25
 #define JETSON_INPUT 33
 
+//I2C stuff
 #define ESP32_I2C_PIN_SDA 23
 #define ESP32_I2C_PIN_SCL 22
 #define I2C_MASTER_FREQ_HZ 400000
@@ -23,6 +28,16 @@
 #define TICKS_BEFORE_TIMEOUT 1
 #define I2C_ACK_EN 0
 #define I2C_BUF_LEN 8
+
+//PWM stuff
+#define MOTOR_PWM_CHANNEL_LEFT LEDC_CHANNEL_1 //attach left motor to ledc channel 1
+#define MOTOR_PWM_CHANNEL_RIGHT LEDC_CHANNEL_2
+#define MOTOR_PWM_TIMER LEDC_TIMER_1
+#define MOTOR_PWM_BIT_NUM LEDC_TIMER_10_BIT
+
+#define RIGHT_PWM_PIN 19
+#define LEFT_PWM_PIN 21
+
 
 #define TASK_STACK_SIZE 512 //default task stack size to use = 512 bytes, idk if we need to change
 
@@ -77,6 +92,49 @@ void ultrasonicTask(void* param) {
     }
 
 }
+
+//setup function for motor pwm signal
+void motor_pwm_init(void)
+{
+    ledc_channel_config_t ledc_channel_left = {0}, ledc_channel_right = {0};
+    ledc_channel_left.gpio_num = LEFT_PWM_PIN;
+    ledc_channel_left.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_channel_left.channel = MOTOR_PWM_CHANNEL_LEFT;
+    ledc_channel_left.intr_type = LEDC_INTR_DISABLE;
+    ledc_channel_left.timer_sel = MOTOR_PWM_TIMER;
+    ledc_channel_left.duty = 0;
+	
+    ledc_channel_right.gpio_num = RIGHT_PWM_PIN;
+    ledc_channel_right.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_channel_right.channel = MOTOR_PWM_CHANNEL_RIGHT;
+    ledc_channel_right.intr_type = LEDC_INTR_DISABLE;
+    ledc_channel_right.timer_sel = MOTOR_PWM_TIMER;
+    ledc_channel_right.duty = 0;
+	
+    ledc_timer_config_t ledc_timer;
+    ledc_timer.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_timer.duty_resolution = MOTOR_PWM_BIT_NUM;
+    ledc_timer.timer_num = MOTOR_PWM_TIMER;
+    ledc_timer.freq_hz = 25000; //25kHz right now, can change
+	
+    ledc_channel_config(&ledc_channel_left);
+    ledc_channel_config(&ledc_channel_right);
+    ledc_timer_config(&ledc_timer);
+}
+
+// //function to set motor pwm signal
+void motor_pwm_set(float left_duty_fraction, float right_duty_fraction) {
+	uint32_t max_duty = (1 << MOTOR_PWM_BIT_NUM) - 1;
+	uint32_t left_duty = lroundf(left_duty_fraction * (float)max_duty);
+	uint32_t right_duty = lroundf(right_duty_fraction * (float)max_duty);
+	
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, MOTOR_PWM_CHANNEL_LEFT, left_duty);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, MOTOR_PWM_CHANNEL_LEFT);
+
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, MOTOR_PWM_CHANNEL_RIGHT, right_duty);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, MOTOR_PWM_CHANNEL_RIGHT);
+}
+
 
 
 static void IRAM_ATTR handleJetsonInput(void* arg) {
@@ -148,20 +206,25 @@ void app_main() {
     memset(buf, 0, 2048);
 
 
-
     gpio_install_isr_service(0);
     gpio_isr_handler_add(JETSON_INPUT, handleJetsonInput, (void*) JETSON_INPUT);
 
     // io_conf.pin_bit_mask = (1ULL << 13);
     // io_conf.mode = GPIO_MODE_OUTPUT;
     // gpio_config(&io_conf);
-
     // int currVal = 0;
+
+    //PWM init
+    motor_pwm_init();
+    motor_pwm_set(0.01, 0.01);
+
     while (1) {
         // currVal = !currVal;
         // gpio_set_level(13, currVal);
         // vTaskDelay(5000 / portTICK_PERIOD_MS);
-        int res = i2c_slave_read_buffer(I2C_NUM_0, (uint8_t*) buf, 2048, 1000 / portTICK_PERIOD_MS);
+
+        //I2C read
+        int res = i2c_slave_read_buffer(I2C_NUM_0, (uint8_t*) buf, 2048, 0);
         printf("Received data: ");
         for (int i = 0; i < 2048; i++) {
             //printf("0x%02X", buf[i]);
@@ -169,8 +232,18 @@ void app_main() {
         }
         printf("\n");
         printf("bytes read: %d\n", res);
+
+        if (buf[0] == 1) {
+            motor_pwm_set(0.9, 0.9);
+        } else if (buf[0] == 2) {
+            motor_pwm_set(0.01, 0.9);
+        } else if (buf[0] == 3) {
+            motor_pwm_set(0.9, 0.01);
+        }
+
         memset(buf, 0, 2048); //reset buffer
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+        vTaskDelay(50 / portTICK_PERIOD_MS);
     }
 
 }
