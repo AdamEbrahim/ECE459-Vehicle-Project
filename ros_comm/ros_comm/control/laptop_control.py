d5f5bc376bf3efa09f84e822dcc8aabb0a2977d1
Alvin Hong <alvin.hong@duke.edu>
2024-11-21 03:35:23 -0500

    update laptop_control to send char

--- ros_comm/ros_comm/control/laptop_control.py
+++ ros_comm/ros_comm/control/laptop_control.py
@@ -3,7 +3,7 @@ import socket
 from pynput import keyboard
 import time
 
-current_command = "FORWARD"
+current_command = "x"  # Start with stop command
 sending_enabled = False  # Flag to indicate whether sending is enabled
 last_print_time = 0  # To track when the last print occurred
 
@@ -12,15 +12,18 @@ def on_press(key):
     
     try:
         if key.char == 't':
-            sending_enabled = True  # Start sending commands when 's' is pressed
+            sending_enabled = True  # Start sending commands when 't' is pressed
+            print("Command sending enabled!")
         if key.char == 'w':
-            current_command = "FORWARD"
+            current_command = 'w'
         elif key.char == 'a':
-            current_command = "LEFT"
+            current_command = 'a'
         elif key.char == 's':
-            current_command = "BACKWARD"
+            current_command = 's'
         elif key.char == 'd':
-            current_command = "RIGHT"
+            current_command = 'd'
+        elif key.char == 'x':
+            current_command = 'x'
         elif key.char == 'q':
             print("Exiting...")
             sending_enabled = False  # Stop sending commands
@@ -32,16 +35,24 @@ def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     server_ip = '10.194.159.230'  
-    server_port = 12000
+    server_port = 12000  
 
     print(f"Connecting to {server_ip}:{server_port}...")
     client_socket.connect((server_ip, server_port))
     print("Connected to the server.")
+    print("\nControls:")
+    print("t - Enable command sending")
+    print("w - Forward")
+    print("s - Backward")
+    print("a - Left")
+    print("d - Right")
+    print("x - Stop")
+    print("q - Quit")
     
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
 
-    global last_print_time  # To access and update the last_print_time
+    global last_print_time
 
     try:
         while True:
@@ -53,9 +64,6 @@ def start_client():
                 if current_time - last_print_time >= 2:
                     print(f"Sending command: {current_command}")
                     last_print_time = current_time  # Update the last print time
-
-                response = client_socket.recv(1024)  # Receive the server's response
-                print(f"Response from server: {response.decode()}")
 
             time.sleep(0.1)  # Small delay to reduce CPU usage
             
