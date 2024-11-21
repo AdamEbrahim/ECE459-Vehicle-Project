8228c841bf21a6c6663ee9dabeb9d2cd02aeb446
Alvin Hong <alvin.hong@duke.edu>
2024-11-21 00:19:00 -0500

    dashing simplify

--- ros_comm/setup.py
+++ ros_comm/setup.py
@@ -1,42 +1,23 @@
 from setuptools import setup
-import os
-from glob import glob
 
 package_name = 'ros_comm'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
-    package_dir={'': '.'},
+    packages=[package_name],  # ros_comm for Python imports
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        ('lib/' + package_name, [
-            'ros_comm/computer_vision/dummy_cv_model.py',
-            'ros_comm/perception/perception_model.py',
-            'ros_comm/control/robot_controller.py',
-            'ros_comm/esp/esp_forwarder2.py',
-            'ros_comm/control/user_control.py'
-        ])
+        ('share/' + package_name, ['package.xml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
-    maintainer_email='user@todo.todo',
+    maintainer='ece459',
+    maintainer_email='ece459@todo.todo',
     description='Robotic Vision Control System',
     license='TODO: License declaration',
     tests_require=['pytest'],
-    scripts=[
-        'ros_comm/computer_vision/dummy_cv_model.py',
-        'ros_comm/perception/perception_model.py',
-        'ros_comm/control/robot_controller.py',
-        'ros_comm/esp/esp_forwarder2.py',
-        'ros_comm/control/user_control.py'
-    ],
     entry_points={
         'console_scripts': [
             'cv_model = ros_comm.computer_vision.dummy_cv_model:main',
